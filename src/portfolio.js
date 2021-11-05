7cf7781a1a0506e11f87195d13176b21c9014f67
Derek Mackley <Derek.Mackley@hotmail.com>
2021-11-05 14:52:22 -0400

    resume site update

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,9 +6,9 @@ import airbnb from "./assets/img/icons/common/airbnbLogo.png";
 
 export const greetings = {
 	name: "Derek Mackley",
-	title: "Hi all, I'm Derek",
+	title: "Hi There, I'm Derek",
 	description:
-		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Python / Django / Flask and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+		"A passionate Full Stack Web Developer with experience of building Web applications and APIs with .Net / C# / Python / JavaScript / Reactjs / Nodejs / Angular / Flask / and tons of other cool libraries and frameworks.",
 	resumeLink: "https://cv.dummY_URl",
 };
 
@@ -29,13 +29,13 @@ export const skillsSection = {
 		"CRAZY FULL STACK DEVELOPER WHO TINKERS WITH ANYTHING I CAN GET MY HANDS ON",
 	skills: [
 		emoji(
-			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+			"⚡ Develop extensable, scaleable, and secure web based applications"
 		),
 		emoji(
-			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+			"⚡ Update and Migrate legacy apps to new frameworks using cutting edge technology"
 		),
 		emoji(
-			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+			"⚡ Learn and play with any technology I can; from automation tools and frameworks to hardware."
 		),
 	],
 
@@ -113,16 +113,16 @@ export const skillsSection = {
 
 export const SkillBars = [
 	{
-		Stack: "Frontend/Design", //Insert stack or technology you have experience in
-		progressPercentage: "70", //Insert relative proficiency in percentage
+		Stack: "Software Architecture", //Insert stack or technology you have experience in
+		progressPercentage: "75",
 	},
 	{
-		Stack: "Backend",
+		Stack: "API layer and below", //Insert relative proficiency in percentage
 		progressPercentage: "90",
 	},
 	{
-		Stack: "Software Architecture",
-		progressPercentage: "60",
+		Stack: "Frontend/Design",
+		progressPercentage: "50",
 	},
 ];
 
@@ -137,60 +137,48 @@ export const educationInfo = [
 
 export const experience = [
 	{
-		role: "Software Engineer",
-		company: "Google",
-		companylogo: googlelogo,
-		date: "June 2018 – Present",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		descBullets: [
-			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-		],
-	},
-	{
-		role: "Front-End Developer",
-		company: "Github",
-		companylogo: github,
-		date: "May 2017 – May 2018",
-		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	},
-	{
-		role: "Software Engineer Intern",
-		company: "Airbnb",
+		role: "Senior Software Engineer",
+		company: "Traction Tools ",
 		companylogo: airbnb,
-		date: "Jan 2015 – Sep 2015",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		date: "February 2021 – Present",
+		desc: "Working as the lead Engineer for a direct-to-consumer site that provides tooling for businesses running EOS. I lead a team of 4 other engineers, doing all code reviews, sprint management, and overseeing the direction of the product from the engineering standpoint. In my first few months I was able to institute Azure Devops, automated deployments, and sprints to the organization. I’m currently working to modernize the tech stack, by methodically migrating the website from .NET Razor and AngualarJS, to a microservices architecture with a react front end.",
 	},
+	{
+		role: "Senior Software Engineer",
+		company: "Ubicquia",
+		companylogo: github,
+		date: "May 2018 – June 2020",
+		desc: "This role involved creating an enterprise solution to manage cellular connected units on light poles. While in this role, I wrote and managed a php Laravel API, with an Angular front end, as well as wrote automation frameworks, MQTT broker connections, python API’s, virtualized environments, and a whole host of supporting software. While in this role I learned to manage and configure Linux environments, and wrote ansible scripts to migrate and re-architect the business from Azure to the Google Cloud Platform. The last one saving the company 500K annually. This role involved a very small team with big tasks, and short deadlines. This taught me how to move quickly, focus on what is truly important, and to write extensible software with manageable tech debt.",
+	},
+	{
+		role: "Software Engineer",
+		company: "Starr Companies",
+		companylogo: googlelogo,
+		date: "March 2016 – May 2018",
+		desc: "Worked on a Document Management/ Reporting web portal. Myself and one other developer rewrote the site in MVC 5 from webforms, moved from an on-prem virtual machine to an azure web service, moved our most resource intensive page to Angular JS, and at the time I left were in the process of updating the front-end to use modern technologies, like Saas, Twitter Bootstrap, NodeJS, etc. In addition, began moving the service layer from .NET4.6.1 to a .NET Core Web API",
+	}
+
 ];
 
 export const projects = [
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		link: "https://example.com",
+		name: "Homelab",
+		desc: "Welcome you're on it, this site is hosted at home! I beleive that in order to be good at this job you have to love it, and you have to play with new stuff.  My homelab consists of more servers than I know what to do with with around 240TB of storage."
 	},
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		github: "https://github.com/DapperDivers",
+		name: "Tinkering with RFID",
+		desc: "I started playing with rfid tags a few years ago, and am in the process of \"removing keys\" from my life.  Ask me about it!"
 	},
 	{
-		name: "lorem ipsum",
-		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-		github: "https://github.com/DapperDivers",
-		link: "https://example.com",
+		name: "3D Printing",
+		desc: "I love taking a idea on a computer and making a physical object! I currently have 4 printers and find alot of joy customizing them.",
 	},
 ];
 
 export const feedbacks = [
 	{
-		name: "Hassan Mehmood",
+		name: "Scott Latsa",
 		feedback:
-			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Derek Mackley has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose Derek web developer services for u.",
-	},
-	{
-		name: "Maheen Altaf",
-		feedback:
-			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
-	},
+		"Derek Mackley reported directly to me at Ubicquia in Melboure Florida and was an invaluable asset to the team, myself and the company.  Derek was a dependable developer that always accomplished the task assigned even in the midst of changing business requirements and priorities.  He was quick to learn new technologies, such as languages, servers, cloud platforms, orchestration systems, and the like. This was despite Derek being from a .NET Windows development environment. He was able to learn PHP Laravel, Python, Debian Linux systems and IOT devices in short order. \n Derek  was an asset to my team. He was always self-motivated and willing to assist with any project. Along with being a top notch developer, he had strong communication and interpersonal skills. While working with Derek I assigned him many tasks with different scopes and technologies and he was able to learn what was needed and produce a working solution in a minimal timeframe.  I enjoyed working with him and would happily work with him in the future. He has my highest recommendation.",
+	}
 ];
